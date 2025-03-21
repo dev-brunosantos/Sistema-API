cddd285f807b27ae87306a4c7c12a1b90858f430
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-21 17:22:05 -0300

    Configurando o arquivo APP para usar as rotas

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import { rotas } from './routes/index.routes';
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
+app.use(rotas)
 
 export { app }
