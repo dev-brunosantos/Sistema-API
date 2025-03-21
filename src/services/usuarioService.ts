3572159c91658d9dedbcea660032ea640bb2ca11
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-21 17:56:15 -0300

    Criando o metodo responsavel por apagar os dados do usuário

--- src/services/usuarioService.ts
+++ src/services/usuarioService.ts
@@ -13,10 +13,10 @@ export class UsuarioService {
 
         if(!usuarioExistente) {
             const novoUsuario = await prisma.Usuario().create({
-                data: { nome, sobrenome, email, senha}
+                data: { nome, email, senha, sobrenome}
             })
 
-            return `O usuário ${novoUsuario.nome.toUpperCase()} ${novoUsuario.sobrenome?.toUpperCase()} foi cadastrado com sucesso.`
+            return `O usuário ${novoUsuario.nome.toUpperCase()} foi cadastrado com sucesso.`
         }
 
         return `Usuário ja cadastrado no sistema.`
@@ -40,6 +40,28 @@ export class UsuarioService {
         return {
             statusCode: 404,
             msg: 'Não existe nenhum usuário cadastrado no sistema.'
+        }
+    }
+
+    async ApagarUsuario(id: string) {
+        const usuarioId = await prisma.Usuario().findMany({
+            where: { id }
+        })
+
+        if(usuarioId) {
+            await prisma.Usuario().delete({
+                where: {id}
+            })
+
+            return {
+                statusCode: 200,
+                msg: `Os dados do usuário  foram apagados.`
+            }
+        }
+
+        return {
+            statusCode: 404,
+            msg: 'Não foi encontrado nenhum usuário vinculado ao ID informado.'
         }
     }
 }
