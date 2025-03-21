926fe645843a51a6785383c7f2cd74a5be313e92
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-21 18:07:23 -0300

    Criando a rota para filtrar usuario por ID

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -5,6 +5,7 @@ const rotas = Router()
 
 rotas.post('/usuarios', usuarioController.Cadastrar)
 rotas.get('/usuarios', usuarioController.Listar)
+rotas.get('/usuarios/:id', usuarioController.Filtrar)
 rotas.delete('/usuarios/apagar/:id', usuarioController.Apagar)
 
 export { rotas } 
