4f42ca2aa9a9d1ec08ee4c215a447ba230cc8516
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-25 10:15:52 -0300

    Aplicando a verificação de autenticação no endpoint de exclusão de dados do usuário

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { usuarioController } from "../controllers/usuarioController";
+import { Auth } from "../middleware/auth";
 
 const rotas = Router()
 
 rotas.post('/usuarios', usuarioController.Cadastrar)
 rotas.get('/usuarios', usuarioController.Listar)
 rotas.get('/usuarios/:id', usuarioController.Filtrar)
-rotas.delete('/usuarios/apagar/:id', usuarioController.Apagar)
+rotas.delete('/usuarios/apagar/:id', Auth, usuarioController.Apagar)
+
+rotas.post('/login', usuarioController.Login)
 
 export { rotas } 
